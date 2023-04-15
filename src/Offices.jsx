7d92083fd06b50995469686270e7d3433c9a0938
Drew Giacomi <andrew.giacomi@gmail.com>
2023-04-15 13:37:04 -0400

    missed useRef import

--- src/Offices.jsx
+++ src/Offices.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import OfficeLocationCard from './OfficeLocationCard';
 import GoogleMaps from './GoogleMaps';
 import officeData from './officeData.json';
